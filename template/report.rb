89f671c29f6d6e98a3b208a1e9a89bf6106f3440
mathiko <my.tears13@gmail.com>
2013-04-28 17:18:06 +0900

    use template method

--- template/report.rb
+++ template/report.rb
@@ -1,44 +1,104 @@
 class Report
   attr_reader :format
 
-  def initialize(format)
+  def initialize
     @title  = 'Monthly Report'
     @text   = [ 'Good', 'The Best' ]
-    @format = format
   end
 
   def output_report
-    format = self.format.to_sym
+    output_start
+    output_head
+    output_body_start
+    output_body
+    output_body_end
+    output_end
+  end
 
-    case format
-    when :plain
-      puts "*** #{@title} ***"
-    when :html
-      puts '<html>'
-      puts '  <head>'
-      puts "  <title>#{@title}</title>"
-      puts '  </head>'
-      puts '  <body>'
-    else
-      raise "Unknown format: #{format}"
-    end
-
+  def output_body
     @text.each do |line|
-      case format
-      when :plain
-        puts line
-      else
-        puts "  <p>#{line}</p>"
-      end
+      output_line line
     end
+  end
 
-    case format
-    when :html
-      puts '  </body>'
-      puts '</html>'
-    end
+  def output_start
+    raise 'Called abstract mathod: output_start'
+  end
+
+  def output_head
+    raise 'Called abstract mathod: output_head'
+  end
+
+  def output_body_start
+    raise 'Called abstract mathod: output_body_start'
+  end
+
+  def output_line(line)
+    raise 'Called abstract mathod: output_line'
+  end
+
+  def output_body_end
+    raise 'Called abstract mathod: output_body_end'
+  end
+
+  def output_end
+    raise 'Called abstract mathod: output_end'
   end
 end
 
-report = Report.new(ARGV[0])
+class HTMLReport < Report
+  def output_start
+    puts '<html>'
+  end
+
+  def output_head
+    puts '  <head>'
+    puts "  <title>#{@title}</title>"
+    puts '  </head>'
+  end
+
+  def output_body_start
+    puts '  <body>'
+  end
+
+  def output_line(line)
+    puts "  <p>#{line}</p>"
+  end
+
+  def output_body_end
+    puts '  </body>'
+  end
+
+  def output_end
+    puts '</html>'
+  end
+end
+
+report = HTMLReport.new
+report.output_report
+
+class PlainTextReport < Report
+  def output_start
+  end
+
+  def output_head
+    puts "*** #{@title} ***"
+    puts
+  end
+
+  def output_body_start
+  end
+
+  def output_line(line)
+    puts line
+  end
+
+  def output_body_end
+  end
+
+  def output_end
+  end
+end
+
+report = PlainTextReport.new
 report.output_report
