64b78eefa1dfa945a178c92f7a06fa0fee87a82b
mathiko <my.tears13@gmail.com>
2013-04-29 11:46:15 +0900

    Rfc: use Proc

--- strategy/formatter.rb
+++ strategy/formatter.rb
@@ -1,43 +1,39 @@
 class Report
   attr_reader :title, :text, :formatter
 
-  def initialize(formatter)
+  def initialize(&formatter)
     @title     = 'Monthly Report'
     @text      = [ 'Good', 'The Best' ]
     @formatter = formatter
   end
 
   def output_report
-    self.formatter.output_report(self)
+    self.formatter.call(self)
   end
 end
 
-class HTMLFormatter
-  def output_report(context)
-    puts '<html>'
-    puts '  <head>'
-    puts "    <title>#{context.title}</title>"
-    puts '  </head>'
-    puts '  <body>'
+HTML_FORMATTER = lambda do |context|
+  puts '<html>'
+  puts '  <head>'
+  puts "    <title>#{context.title}</title>"
+  puts '  </head>'
+  puts '  <body>'
 
-    context.text.each do |line|
-      puts "    <p>#{line}</p>"
-    end
+  context.text.each do |line|
+    puts "    <p>#{line}</p>"
+  end
 
-    puts '  </body>'
-    puts '</html>'
+  puts '  </body>'
+  puts '</html>'
+end
+
+PLAIN_TEXT_FORMATTER = lambda do |context|
+  puts "***** #{context.title} *****"
+
+  context.text.each do |line|
+    puts line
   end
 end
 
-class PlainTextFormatter
-  def output_report(context)
-    puts "***** #{context.title} *****"
-
-    context.text.each do |line|
-      puts line
-    end
-  end
-end
-
-report = Report.new(HTMLFormatter.new)
+report = Report.new &HTML_FORMATTER
 report.output_report
