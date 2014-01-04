df288b9ecf8b1033fcf67d2cdfe7d4531dfd3b36
mathiko <my.tears13@gmail.com>
2014-01-04 18:56:06 +0900

    Add Decorator pattern code sample

---
+++ decorator/simple_writer.rb
@@ -0,0 +1,46 @@
+class SimpleWriter
+  def initialize(path)
+    @file = File.open(path, "w")
+  end
+
+  def write_line(line)
+    @file.print(line)
+    @file.print("\n")
+  end
+
+  def pos
+    @file.pos
+  end
+
+  def rewind
+    @file.rewind
+  end
+
+  def close
+    @file.close
+  end
+end
+
+require 'forwardable'
+
+class WriterDecorator
+  extend Forwardable
+
+  def_delegators :@real_writer, :write_line, :rewind, :pos, :close
+
+  def initialize(real_writer)
+    @real_writer = real_writer
+  end
+end
+
+class NumberingWriter < WriterDecorator
+  def initialize(real_writer)
+    super
+    @line_number = 1
+  end
+
+  def write_line(line)
+    @real_writer.write_line "#{@line_number}: #{line}"
+    @line_number += 1
+  end
+end
